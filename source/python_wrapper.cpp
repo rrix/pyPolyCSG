830bceb6d5dbf2029efea37e725c756dc156c094
Ryan Rix <ry@n.rix.si>
2013-02-19 15:40:41 -0800

    add extrusions and cylinders

--- source/python_wrapper.cpp
+++ source/python_wrapper.cpp
@@ -1,4 +1,5 @@
 #include<boost/python.hpp>
+#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
 using namespace boost::python;
 
 #include"polyhedron.h"
@@ -8,12 +9,14 @@ BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS( make_box_overloads,			initialize_create_
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS( make_cylinder_overloads,    initialize_create_cylinder,  2, 4 );
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS( make_cone_overloads,		initialize_create_cone,      2, 4 );
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS( make_torus_overloads,		initialize_create_torus,     2, 5 );
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS( make_extrusion_overloads,		initialize_create_extrusion,     3,3 );
 
 BOOST_PYTHON_FUNCTION_OVERLOADS( sphere_overloads,   sphere,   1, 4 );
 BOOST_PYTHON_FUNCTION_OVERLOADS( box_overloads,      box,      3, 4 );
 BOOST_PYTHON_FUNCTION_OVERLOADS( cylinder_overloads, cylinder, 2, 4 );
 BOOST_PYTHON_FUNCTION_OVERLOADS( cone_overloads,     cone,     2, 4 );
 BOOST_PYTHON_FUNCTION_OVERLOADS( torus_overloads,    torus,    2, 5 );
+BOOST_PYTHON_FUNCTION_OVERLOADS( extrusion_overloads,    extrusion,    3,3 );
 
 BOOST_PYTHON_MODULE(pyPolyCSG){
 	numeric::array::set_module_and_type("numpy", "ndarray");
@@ -24,6 +27,7 @@ BOOST_PYTHON_MODULE(pyPolyCSG){
 	def( "cylinder",	     cylinder,  cylinder_overloads() );
 	def( "cone",		     cone,      cone_overloads() );
 	def( "torus",		     torus,     torus_overloads() );
+	def( "extrusion",		     extrusion,     extrusion_overloads() );
 	
 	class_<polyhedron>("polyhedron")
 	.def( "load_mesh",	               &polyhedron::initialize_load_from_file )
@@ -31,6 +35,8 @@ BOOST_PYTHON_MODULE(pyPolyCSG){
 	.def( "make_box",                  &polyhedron::initialize_create_box,    make_box_overloads() )
 	.def( "make_cone",                 &polyhedron::initialize_create_cone,   make_cone_overloads() )
 	.def( "make_torus",                &polyhedron::initialize_create_torus,  make_torus_overloads() )
+	.def( "make_cylinder",                &polyhedron::initialize_create_cylinder,  make_cylinder_overloads() )
+	.def( "make_extrusion",                &polyhedron::initialize_create_extrusion,  make_extrusion_overloads() )
 	.def( "translate",                 &polyhedron::translate )
 	.def( "rotate",                    &polyhedron::rotate )
 	.def( "scale",                     &polyhedron::scale )
@@ -48,4 +54,10 @@ BOOST_PYTHON_MODULE(pyPolyCSG){
 	.def( self - polyhedron() )
 	.def( self * polyhedron() )
 	;
+
+boost::python::class_<std::vector<double> >("DoubleVec")
+  .def(boost::python::vector_indexing_suite<std::vector<double> >());
+boost::python::class_<std::vector<int> >("IntVec")
+  .def(boost::python::vector_indexing_suite<std::vector<int> >());
 }
+
